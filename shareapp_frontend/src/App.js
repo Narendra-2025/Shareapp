8c2655a0b8794dc900387d0c076c84781c929773
Narendra-2025 <nk195485747@gmail.com>
2025-06-17 12:29:16 +0530

    changes

--- shareapp_frontend/src/App.js
+++ shareapp_frontend/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom';
+import React, { useEffect } from 'react'
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import Login from './Component/Login.jsx';
 import Home from './Container/Home';
+import { fetchUser } from './utils/fetchUser.js';
 const App = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const user = fetchUser();
+    if(!user) navigate('/login');
+    
+  }, [])
+  
   return (
     <Routes>
       <Route path='login' element= {<Login />}/>
